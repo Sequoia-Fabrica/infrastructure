9f46e8f18ab7a7f70df6adf8f4b2619907fb7688
Jonathan Lassoff <jof@thejof.com>
2025-08-31 20:34:56 -0700

    multipass: first pass at a basic ID webapp

---
+++ multipass/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./web/templates/**/*.html",
+    "./web/static/js/**/*.js"
+  ],
+  darkMode: 'media', // or 'class' for manual dark mode toggling
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
